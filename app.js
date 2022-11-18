66bf36ed2604e842f62b91f1d5a530e7f48f80ea
gnzlplcs <palaciosgm@outlook.com>
2022-11-18 13:04:02 -0500

    init: tsconfig

---
+++ app.js
@@ -0,0 +1,6 @@
+"use strict";
+const msg = 'Hola Mundo';
+const age = 21;
+const logArray = (arr) => arr.map(el => console.log(el));
+const result = logArray(['gonzalo', 'patricia', 'lia', 'nadia', 'joaquin']);
+console.log(result);
---
+++ app.ts
@@ -0,0 +1,7 @@
+const msg: string = 'Hola Mundo';
+const age: number = 21;
+
+const logArray
+= (arr: string[]) => arr.map(el => console.log(el))
+const result = logArray(['gonzalo', 'patricia', 'lia', 'nadia', 'joaquin'])
+console.log(result);
